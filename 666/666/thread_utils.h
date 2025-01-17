8a630c075cadd5d0ca8b1826c794fb339f5c6065
my old account https://github.com/imareev <165126155+IvanMareev@users.noreply.github.com>
2025-01-17 14:28:37 +1000

    Update thread_utils.h

--- 666/666/thread_utils.h
+++ 666/666/thread_utils.h
@@ -6,12 +6,16 @@
 unsigned g_num_threads;
 
 void set_num_threads_auto() {
-    g_num_threads = omp_get_max_threads();
-    omp_set_num_threads(g_num_threads);
+    g_num_threads = omp_get_max_threads(); 
+    omp_set_num_threads(g_num_threads); 
 }
 
+void set_num_threads(unsigned T) {
+    g_num_threads = T;
+};
+
 unsigned get_num_threads() {
-    return g_num_threads;
+    return g_num_threads; 
 }
 
 #endif 
